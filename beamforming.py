cf7cb9cfe47ebf31a5eede6f59390fe01c5e7d34
CsongorVincze <vinczefarkascsongor.com>
2025-09-27 17:15:45 +0200

    halooo

--- beamforming.py
+++ beamforming.py
@@ -1,11 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
 # ? a program a terracsot ugy hozza letre h minden koordinata egesz lesz
 # ? ez lehet h problemas ha pontos egysegekben akarunk szamolni
 
-# T = np.arange(0, 10, 0.01)
-dt = 0.1
+dt = 0.01
+t = np.arange(0, 1, dt)
 c = 340  # m/s hangsebesseg
 f = 5  # hz frekvencia
 res = 0.1  # a felbontasa a ternek
@@ -13,9 +12,6 @@ space_dims = np.array([100.0, 100.0])  # a ter merete amiben dolgozunk
 coord = np.zeros(
     (int(space_dims[0] / res), int(space_dims[1] / res)), dtype=float
 )  # minden pont koordinataja
-
-img = plt.imshow(coord, cmap="plasma")
-plt.colorbar()
 
 s_coord = np.array(
     [3 / res, 5 / res]
@@ -33,19 +29,23 @@ def Single_sound_source(f, c, coord, t, fi, s_coord):  # this is a sinusoidal so
             z = np.sin(
                 2 * np.pi * f / c * dist - 2 * np.pi * f * t - fi
             )  # ? csekk h jo-e (ez adja meg a kiterest)
-            coord[j, i] = z  #! erre itt figyelj
-    return coord
+            coord[j, i] = z  # todo ide majd vissza kell rakni a +-t
 
+
+# MIKROFONOK CUCCAI
 
 center_mic = np.array([70 / res, 60 / res])  # a mikrofonkor kozepe
 num_mics = 7  # hany mikrofonnal dolgozunk
 mic_radius = 5 / res  # mekkor a kor sugara
 
 mic_coord = np.zeros((num_mics, 2))
-mic_results = np.zeros((num_mics,))
+mic_results = np.zeros((num_mics, len(t)))
+print(mic_results)
 
 
-def Mic_array(center_mic, num_mics, mic_coord, mic_results):
+def Mic_array(
+    center_mic, num_mics, mic_coord, mic_results
+):  # a mikrofonok pozicionalasa
     for i in range(num_mics):
         mic_coord[i, 0] = int(
             center_mic[0] - np.sin(i * 2 * np.pi / num_mics) * mic_radius
@@ -54,31 +54,32 @@ def Mic_array(center_mic, num_mics, mic_coord, mic_results):
             center_mic[1] + np.cos(i * 2 * np.pi / num_mics) * mic_radius
         )
 
-        mic_results[i] = coord[int(mic_coord[i, 0]), int(mic_coord[i, 1])]
-        print(f"{mic_results[i]:.2f}")
+
+def Mic_results(mic_results, mic_coord, t_):  # a mikrofonok altal vett jelek gyujtese
+    # for i in range(num_mics):
+    mic_results[i, t_] = coord[int(mic_coord[i, 0]), int(mic_coord[i, 1])]
+    # print(f"{mic_results[i, t_]:.2f}")
 
 
-# for i in range(10):
-#     nana = Single_sound_source(f, c, coord, i * dt, 0, s_coord)
-#     print(nana)
-#     plt.imshow(nana)
-#     plt.show()
+Mic_array(
+    center_mic, num_mics, mic_coord, mic_results
+)  # kiszamoljuk h hol vannak a mikrofonok
+
+fig, axis = plt.subplots(2)
+
+for j in range(len(t)):
+    print(j)
+    Single_sound_source(f, c, coord, t[j], 0, s_coord)
+    Mic_results(mic_results, mic_coord, j)
+
+print(mic_results)
+for i in range(num_mics):
+    axis[1].plot(t, mic_results[i])
 
 
-def update(frame):
-    img.set_array(Single_sound_source(f, c, coord, frame * dt, 0, s_coord))
-    return (img,)
+im = axis[0].imshow(coord, cmap="inferno")
+fig.colorbar(im, ax=axis[0])
+axis[0].scatter(mic_coord[:, 0], mic_coord[:, 1], c="#79E614", marker="x")
 
-
-animation = FuncAnimation(plt.gcf(), update, frames=50, interval=dt * 10)
+plt.legend()
 plt.show()
-
-
-# Mic_array(center_mic, num_mics, mic_coord, mic_results)
-
-
-# plt.imshow(coord, cmap="inferno")
-# plt.colorbar()
-# plt.scatter(mic_coord[:, 0], mic_coord[:, 1], c="#79E614", marker="x")
-# plt.legend()
-# plt.show()
